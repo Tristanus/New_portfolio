0df193e96c64f7e06123c0f01f72a93fd174218a
DESKTOP-S0MLQRE\Tristan <info@tristandejager.com>
2017-02-15 12:45:03 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+	<?php
+
+		include "include/head.php";
+
+	?>
+
+	<body>
+
+		<div id="home" class="fullHeight tCenter">
+
+			<a class="about show linkNav white" href="#">about me</a>
+
+			<div class="center">
+				<div class="centerKid white">
+
+					<h1>Tristan de Jager</h1>
+					<h2>Upcoming Web Developer</h2>
+					<a href="https://lmgtfy.com/?q=tristan+de+jager" target="_blank"><h3>Google my ass...</h3></a>
+
+				</div>
+			</div>	
+
+			<a class="work show linkNav white" href="#">work</a>
+
+
+		</div>
+
+	</body>
+
+</html>
